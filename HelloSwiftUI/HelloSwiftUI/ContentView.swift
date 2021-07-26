6a7d97640bf50455c422e84d7d78a0089ce5c900
TonyJem <a.jemanov@gmail.com>
2021-07-26 21:18:17 +0300

    Update ContentView.swift

--- HelloSwiftUI/HelloSwiftUI/ContentView.swift
+++ HelloSwiftUI/HelloSwiftUI/ContentView.swift
@@ -4,26 +4,36 @@ struct ContentView: View {
     var body: some View {
         Form {
             Section(header: Text("Section 1")) {
-                Text("Element 1.1")
-                Text("Element 1.2")
+                Text("Это текст в первой секции")
+                Text("И это тоже")
             }
             Section(header: Text("Section 2")) {
-                Text("Element 2.1")
-                Text("Element 2.2")
+                Text("А это уже текст второй секции")
+                Text("Да да")
             }
             Section(header: Text("Section 3")) {
-                Text("Element 3.1")
-                Text("Element 3.2")
+                Text("Здесь третья секция")
+                Text("Дальше идет перечисление цветов")
             }
             Section() {
-                Text("Element 4.1")
-                Text("Element 4.2")
-                Text("Element 4.3")
-                Text("Element 4.4")
-                Text("Element 4.5")
-                Text("Element 4.6")
-                Text("Element 4.7")
-                Text("Element 4.8")
+                Text("Зеленый")
+                    .foregroundColor(.green)
+                Text("Розовый")
+                    .foregroundColor(.pink)
+                Text("Жёлтый")
+                    .foregroundColor(.yellow)
+                Text("Красный")
+                    .foregroundColor(.red)
+                Text("Синий")
+                    .foregroundColor(/*@START_MENU_TOKEN@*/.blue/*@END_MENU_TOKEN@*/)
+                Text("Серый")
+                    .foregroundColor(.gray)
+                Text("Оранжевый")
+                    .foregroundColor(.orange)
+                Text("Фиолетовый")
+                    .foregroundColor(.purple)
+                Text("Синий")
+                    .foregroundColor(/*@START_MENU_TOKEN@*/.blue/*@END_MENU_TOKEN@*/)
             }
         }
     }
@@ -34,3 +44,4 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
