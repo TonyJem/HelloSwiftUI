b693971e8ac405a98f1669f1fae077ef28e1b3c6
TonyJem <a.jemanov@gmail.com>
2021-07-26 21:09:23 +0300

    Create Form

--- HelloSwiftUI/HelloSwiftUI/ContentView.swift
+++ HelloSwiftUI/HelloSwiftUI/ContentView.swift
@@ -2,8 +2,30 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, My SwiftUI!")
-            .padding()
+        Form {
+            Section(header: Text("Section 1")) {
+                Text("Element 1.1")
+                Text("Element 1.2")
+            }
+            Section(header: Text("Section 2")) {
+                Text("Element 2.1")
+                Text("Element 2.2")
+            }
+            Section(header: Text("Section 3")) {
+                Text("Element 3.1")
+                Text("Element 3.2")
+            }
+            Section() {
+                Text("Element 4.1")
+                Text("Element 4.2")
+                Text("Element 4.3")
+                Text("Element 4.4")
+                Text("Element 4.5")
+                Text("Element 4.6")
+                Text("Element 4.7")
+                Text("Element 4.8")
+            }
+        }
     }
 }
 
